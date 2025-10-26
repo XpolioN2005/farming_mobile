162c8cf58a1db61a5fb48dab104a57eb4b7a5f16
XpolioN <146839835+XpolioN2005@users.noreply.github.com>
2025-10-26 19:30:56 +0530

    feat: Add worker dragging and root scene icon

--- scripts/worker/base_worker.gd
+++ scripts/worker/base_worker.gd
@@ -1,1 +1,16 @@
 extends CharacterBody2D
+
+var is_dragging : bool = false
+var of : Vector2 = Vector2.ZERO
+
+func _process(_delta):
+	if is_dragging:
+		position = get_global_mouse_position()- of
+
+func _on_button_button_up() -> void:
+	is_dragging = false
+
+
+func _on_button_button_down() -> void:
+	is_dragging = true
+	of = get_global_mouse_position()-global_position
